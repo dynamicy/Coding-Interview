d1c060dbc23a313f183b8a95c011a0491a1f0147
山崎ヒカル <fermat.little.theorem@gmail.com>
2023-06-10 18:21:21 +0000

    Array Change AC.

---
+++ codesignal/typescript/ArrayChange/array-change.ts
@@ -0,0 +1,11 @@
+function solution(inputArray: number[]): number {
+    let move = 0;    
+    for (let i = 1; i<inputArray.length; i++) {
+        if (inputArray[i] <= inputArray[i-1]) {            
+            const temp = inputArray[i];
+            inputArray[i] = inputArray[i-1] + 1;
+            move += inputArray[i] - temp;
+        }
+    }
+    return move;
+}
---
+++ codesignal/typescript/ArrayChange/exampl.md
@@ -0,0 +1,4 @@
+Example
+
+For inputArray = [1, 1, 1], the output should be
+solution(inputArray) = 3.
