5a4becb0a09d0b4d45a5208fbfabc4ea6f719925
山崎ヒカル <fermat.little.theorem@gmail.com>
2023-06-11 07:12:21 +0000

    27 Remove Element AC.

---
+++ leetcode/typescript/27_RemoveElement/remove-element.ts
@@ -0,0 +1,10 @@
+function removeElement(nums: number[], val: number): number {
+    let size = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[size] = nums[i];
+            size++;
+        }
+    }
+    return size;
+};
