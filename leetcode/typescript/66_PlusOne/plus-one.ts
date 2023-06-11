5232c547460f51b5f3c2ca6eb69af8c3038f6518
山崎ヒカル <fermat.little.theorem@gmail.com>
2023-06-11 09:40:04 +0000

    Fix 66 Plus One by looping.

--- leetcode/typescript/66_PlusOne/plus-one.ts
+++ leetcode/typescript/66_PlusOne/plus-one.ts
@@ -1,5 +1,15 @@
 function plusOne(digits: number[]): number[] {    
-    let inputNumber = parseInt(digits.join(''));
-    inputNumber += 1;
-    return String(inputNumber).split('').map(Number);
+    // 123 => 124
+    // 129 => 120 => 130
+    // 199 => 190 => 100 => 200
+    // 999 => 990 => 900 => 000 => 1000
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+    digits.unshift(1);
+    return digits;
 };
