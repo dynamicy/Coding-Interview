089ccf3dbcd155bd362b97e4aebc3ea0ec4a5ce7
山崎ヒカル <fermat.little.theorem@gmail.com>
2023-06-22 02:53:08 +0000

    206 Reverse Linked List AC.

---
+++ leetcode/kotlin/206_ReverseLinkedList/ReverseLinkedList.kt
@@ -0,0 +1,23 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun reverseList(head: ListNode?): ListNode? {
+        var current: ListNode? = head
+        var prev: ListNode? = null
+
+        while (current != null) {
+            val next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        }
+        return prev
+    }
+}
