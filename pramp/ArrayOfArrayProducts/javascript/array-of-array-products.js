725edc069da16ea10cada9962e69ecb3c0264a6b
chris <chris@csie.io>
2024-05-17 11:49:00 +0800

    refactoring

--- pramp/ArrayOfArrayProducts/javascript/array-of-array-products.js
+++ pramp/ArrayOfArrayProducts/javascript/array-of-array-products.js
@@ -1,27 +1,28 @@
 function arrayOfArrayProducts(arr) {
     // your code goes here
+    let isZeroIncluded = false;
+    let product = 1;
+    let result = [];
+
     if (arr.length <= 1) {
       return [];
     }
-    
-    let product = 1;
-    let isZeroIncluded = false;
-    const result = [];
-  
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] != 0) {
-        product *= arr[i];      
-      } else {
+
+    arr.forEach((item) => {
+      if (item === 0) {
         isZeroIncluded = true;
-      }
-    }
-    
-    for (let i = 0; i< arr.length; i++) {
-      if (isZeroIncluded === true) {
-        result[i] = arr[i] != 0 ? 0 : product;
       } else {
-        result[i] = product / arr[i];
+        product *= item;
       }
-    }
+    });
+
+    arr.forEach((item) => {
+      if (isZeroIncluded) {
+        result.push(item === 0 ? product : 0)
+      } else {
+        result.push(product / item);
+      }
+    });
+
     return result;
   }
